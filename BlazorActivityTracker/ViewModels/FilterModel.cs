65417f2a98f78a25e790ce166dc1472dfacd2260
TA <tadams03@mail.roosevelt.edu>
2023-10-27 19:53:03 -0500

    Add UIs

---
+++ BlazorActivityTracker/ViewModels/FilterModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorActivityTracker.ViewModels
+{
+    public class FilterModel
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
